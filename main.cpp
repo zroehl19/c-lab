a9b1707c55deb9f2b47a35d5d8b0d5d04bff0c92
Zach Roehl <zroehl19@gmail.com>
2017-09-14 12:38:23 -0400

    final

---
+++ main.cpp
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: zroehl
+ *
+ * Created on September 12, 2017, 1:57 PM
+ */
+
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main() {
+    int vector[] = {3, -5, 7, 10, -4, 14, 5, 2, -13};
+    int n = sizeof(vector)/sizeof(vector[0]);
+    
+    int smallest;
+    int *p1;
+    
+    p1 = vector;
+    smallest = *p1;
+    
+    for(int i; i < n; i++)
+    {
+        if(*p1 > smallest)
+        {
+            *p1 = smallest;
+            
+            p1++;
+        }
+        else
+        {
+            p1++;
+        }
+    }
+    
+    cout << *p1;
+}
+
